36433ed1883f58756327fec584419fd65bf84fa4
agostina-rosso <agostina.partenopea@gmail.com>
2025-11-13 19:29:40 +0100

    Edited API key

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "7e43t102ob02c30d9fabf0c6b85d4a1a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }

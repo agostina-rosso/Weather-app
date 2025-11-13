ed1adb566ad8e09bfaab2b5cbfc566e47380d03f
agostina-rosso <agostina.partenopea@gmail.com>
2025-11-13 18:26:35 +0100

    EDITION

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-
-  cityElement.innerHTML = response.data.city;
+  cityElement.innerHTML = searchInput.value;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 function searchCity(city) {
@@ -16,7 +16,8 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
